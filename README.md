cc58af2b86abc50c97ef0cc9dbaeb49455cac1d3
bhartlenn <bhartlenn@gmail.com>
2022-07-26 10:18:16 -0700

    First commit of project files

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+This is a very simple boilerplate rails 7 app that includes a simple pages controller for pages like a homepage, and it has devise installed, and setup to handle turbo style flash messages.
+
+* Ruby version: 3.1.2
+
+* Rails version: 7.0.3.1
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
+end
