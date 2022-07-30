13692d330a3d70bd5b77ae62ed9b5da9877ed1b3
bhartlenn <bhartlenn@gmail.com>
2022-07-30 13:55:56 -0700

    Updated README.md to describe how flash messages are setup to work in future controller actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+    def render_turbo_stream_flash_messages
+        turbo_stream.prepend "flashes", partial: "layouts/flash"
+    end
+
     def controller_and_action_string
         controller_name + "_" + action_name
     end
